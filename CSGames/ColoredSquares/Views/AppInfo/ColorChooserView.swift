0eb3acb41c6a1aa8e213d6c5f670f508810415ca
volker <101987062+gruendau@users.noreply.github.com>
2023-07-06 23:48:26 +0200

    Renamed CSColorChoser, CSColorCloseSquare, CSColor and moved CSColorChoseSquare in CSColorChoser.

--- CSGames/ColoredSquares/Views/AppInfo/ColorChooserView.swift
+++ CSGames/ColoredSquares/Views/AppInfo/ColorChooserView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct SwiftUIColor: Identifiable {
+struct CSColor: Identifiable {
     var id: UUID
     let name: String
     let color: Color
@@ -19,74 +19,32 @@ struct SwiftUIColor: Identifiable {
     }
 }
 
-let swiftUIColors = [
-    SwiftUIColor(".black", .black),
-    SwiftUIColor(".white", .white),
-    SwiftUIColor(".red", .red),
-    SwiftUIColor(".green", .green),
-    SwiftUIColor(".blue", .blue),
-    SwiftUIColor(".yellow", .yellow),
-    SwiftUIColor(".purple", .purple),
-    SwiftUIColor(".cyan", .cyan),
-    SwiftUIColor(".orange", .orange),
-    SwiftUIColor(".pink", .pink),
-    SwiftUIColor(".mint", .mint),
-    SwiftUIColor(".indigo", .indigo),
-    SwiftUIColor(".brown", .brown),
-    SwiftUIColor(".teal", .teal),
-    SwiftUIColor(".gray", .gray),
-//    SwiftUIColor(".primary", .primary),
-//    SwiftUIColor(".secondary", .secondary),
-//    SwiftUIColor(".accentColor", .accentColor)
+let colors = [
+    CSColor(".black", .black),
+    CSColor(".white", .white),
+    CSColor(".red", .red),
+    CSColor(".green", .green),
+    CSColor(".blue", .blue),
+    CSColor(".yellow", .yellow),
+    CSColor(".purple", .purple),
+    CSColor(".cyan", .cyan),
+    CSColor(".orange", .orange),
+    CSColor(".pink", .pink),
+    CSColor(".mint", .mint),
+    CSColor(".indigo", .indigo),
+    CSColor(".brown", .brown),
+    CSColor(".teal", .teal),
+    CSColor(".gray", .gray),
+//    CSColor(".primary", .primary),
+//    CSColor(".secondary", .secondary),
+//    CSColor(".accentColor", .accentColor)
 ]
 
-struct ColorChooseSquare: View {
-    
-    let color: Color
-    let borderColor: Color
-    @Binding var bindedColor: Color
-    
-    @EnvironmentObject var csm: ColoredSquaresModel
-    @EnvironmentObject var vp: ViewParameter
-    
-    @State var isHovered = false
-
-    
-//    init (color: Color = .red,
-//          borderColor: Color = .white,
-//          newColor: Binding<Color>) {
-//        self.color = color
-//        self.borderColor = borderColor
-//        self.newColor = newColor
-//    }
-        
-    var body: some View {
-        
-        ZStack {
-            
-            // Border of color chooser square for active color
-            Rectangle()
-                .frame(width: 14 + 4, height: 14 + 4)
-                .foregroundColor(vp.screenBackgroundColor == . red ? .blue : .red)
-                .padding(-3)
-                .opacity(color == bindedColor ? 1 : 0)
-            
-            // Interior of color chooser square
-            Rectangle()
-                .frame(width: 14, height: 14)
-                .border(vp.screenBackgroundColor == . red ? .blue : .red)
-                .padding(-3)
-                .onTapGesture() { bindedColor = color }
-                .foregroundColor(isHovered ? .green : color)
-                .onHover { over in isHovered = over }
-                
-        }
-    }
-}
 
 
 
-struct ColorChooser: View {
+
+struct CSColorChoser: View {
     
     let text: String
     @Binding var bindedColor: Color
@@ -102,8 +60,8 @@ struct ColorChooser: View {
             
             HStack {
                 
-                ForEach(swiftUIColors[0..<swiftUIColors.count]) {
-                    ColorChooseSquare(color: $0.color,
+                ForEach(colors[0..<colors.count]) {
+                    CSColorChoseSquare(color: $0.color,
                                       borderColor: .white,
                                       bindedColor: $bindedColor)
                                       //bindedColor: $vp.screenBackgroundColor)
@@ -115,15 +73,62 @@ struct ColorChooser: View {
             
         }
     }
+    
+    
+    // MARK: CSColorChoseSquare
+    struct CSColorChoseSquare: View {
+        
+        let color: Color
+        let borderColor: Color
+        @Binding var bindedColor: Color
+        
+        @EnvironmentObject var csm: ColoredSquaresModel
+        @EnvironmentObject var vp: ViewParameter
+        
+        @State var isHovered = false
+
+        
+    //    init (color: Color = .red,
+    //          borderColor: Color = .white,
+    //          newColor: Binding<Color>) {
+    //        self.color = color
+    //        self.borderColor = borderColor
+    //        self.newColor = newColor
+    //    }
+            
+        var body: some View {
+            
+            ZStack {
+                
+                // Border of color chooser square for active color
+                Rectangle()
+                    .frame(width: 14 + 4, height: 14 + 4)
+                    .foregroundColor(vp.screenBackgroundColor == . red ? .blue : .red)
+                    .padding(-3)
+                    .opacity(color == bindedColor ? 1 : 0)
+                
+                // Interior of color chooser square
+                Rectangle()
+                    .frame(width: 14, height: 14)
+                    .border(vp.screenBackgroundColor == . red ? .blue : .red)
+                    .padding(-3)
+                    .onTapGesture() { bindedColor = color }
+                    .foregroundColor(isHovered ? .green : color)
+                    .onHover { over in isHovered = over }
+                    
+            }
+        }
+    }
+    
 }
 
 
-struct ColorChooser_Previews: PreviewProvider {
+struct CSColorChoser_Previews: PreviewProvider {
     
     @State static var bindedColor: Color = .white
     
     static var previews: some View {
-        ColorChooser(text: "Test Color", bindedColor: $bindedColor)
+        CSColorChoser(text: "Test Color", bindedColor: $bindedColor)
             .environmentObject(ColoredSquaresModel())
             .environmentObject(ViewParameter())
     }
