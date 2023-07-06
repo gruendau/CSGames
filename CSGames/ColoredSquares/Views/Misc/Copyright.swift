59aebc539105e1faa76c6ee3df994cbc20898811
volker <101987062+gruendau@users.noreply.github.com>
2023-07-07 01:05:15 +0200

    Clean up.

--- CSGames/ColoredSquares/Views/Misc/Copyright.swift
+++ CSGames/ColoredSquares/Views/Misc/Copyright.swift
@@ -27,27 +27,8 @@ struct Copyright: View {
         
 }
 
-//struct Copyright {
-//    var text: String = "© 2022 by volker"
-//    var font: Font = .footnote
-//    var color: Color = .red
-//    var opacity: CGFloat = 0.8
-//    var paddingBottom: CGFloat = 30
-//
-////    init (text: String = "© 2022 by volker",
-////          font: Font = .footnote,
-////          color: Color = .red,
-////          opacity: CGFloat = 0.8,
-////          paddingBottom: CGFloat = 30) {
-////        self.text = text
-////        self.font = font
-////        self.color = color
-////        self.opacity = opacity
-////        self.paddingBottom = paddingBottom
-////    }
-//}
 
-struct CopyrightView_Previews: PreviewProvider {
+struct Copyright_Previews: PreviewProvider {
     static var previews: some View {
         Copyright()
             .environmentObject(ViewParameter())
