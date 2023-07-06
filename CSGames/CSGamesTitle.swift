50accb22b894dd0927a639811fc55843839b3360
volker <101987062+gruendau@users.noreply.github.com>
2023-07-06 22:37:46 +0200

    Removed foregroundGradient1 modifier

--- CSGames/CSGamesTitle.swift
+++ CSGames/CSGamesTitle.swift
@@ -7,31 +7,12 @@
 
 import SwiftUI
 
-struct ForegroundGradient1: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .gradientForeground(colors: [
-                CSColors(withoutBlack: true).color,
-                CSColors(withoutBlack: true).color,
-                CSColors(withoutBlack: true).color])
-        
-            .shadow(color: CSColors(withoutBlack: true).color,
-                    radius: 1.3, x: -1, y: 1)
-    }
-}
-
-extension View {
-    func foregroundGradient1 () -> some View {
-        modifier(ForegroundGradient1())
-    }
-}
 
 struct CSGamesTitle: View {
     
     @State var refreshTitle = 1
     
     var body: some View {
-        
         
         HStack {
             Button(action: {
@@ -41,19 +22,6 @@ struct CSGamesTitle: View {
                     .bold()
                     .font(.largeTitle)
                     .foregroundGradient1()
-                
-//                    .foregroundColor(.white)
-//
-//                    .foregroundColor(CSColors(withoutBlack: true).color)
-//
-//                    .gradientForeground(colors:
-//                            [CSColors(withoutBlack: true).color,
-//                             CSColors(withoutBlack: true).color,
-//                             CSColors(withoutBlack: true).color])
-//
-//                    .background(LinearGradient(gradient: Gradient(colors: [.red, .orange, .yellow, .green, .blue]), startPoint: .leading, endPoint: .trailing))
-//
-//                    .shadow(color: CSColors(withoutBlack: true).color, radius: 1.3, x: -1, y: 1)
             })
             
             Text("\(refreshTitle)").hidden()
@@ -61,6 +29,7 @@ struct CSGamesTitle: View {
         
     }
 }
+
 
 struct CSGamesTitle_Previews: PreviewProvider {
     static var previews: some View {
