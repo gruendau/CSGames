02ed1dafccbb511fbf5c8701b2ed7a31756c5588
volker <101987062+gruendau@users.noreply.github.com>
2023-07-07 09:46:38 +0200

    Added Settings.

--- CSGames/CSGamesApp.swift
+++ CSGames/CSGamesApp.swift
@@ -13,12 +13,14 @@ struct CSGamesApp: App {
     
     @StateObject var csm = CSModel()
     @StateObject var vp = CSSettings()
+    @StateObject var s = Settings()
     
     var body: some Scene {
         WindowGroup {
             Main()
                 .environmentObject(csm)
                 .environmentObject(vp)
+                .environmentObject(s)
         }
     }
 }
