5b759e61a81860d140d0e0c1d4b2d7acbe70d993
volker <101987062+gruendau@users.noreply.github.com>
2023-07-07 19:02:35 +0200

    Added some properties, refactored.

--- CSGames/Settings.swift
+++ CSGames/Settings.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+
 class Settings: ObservableObject {
     
     // Texts
@@ -15,10 +16,29 @@ class Settings: ObservableObject {
     
     // Colors
     let colorBackground = Color.black
+    let colorForeground = Color.white
     
     // Positions
-    let paddingTitleTop: CGFloat = 90
-    let paddingCopyrightBottom: CGFloat = 30
+    let paddingTitleTop = CGFloat(90)
+    let paddingCopyrightBottom = CGFloat(30)
+    
+    // Games
+    @Published var isAcitive_ColoredSquares = false
+    
+//    func activateGame (_ game: Games) {
+//
+//        // Deactivate
+//        deactivateAllGames()
+//
+//        // Activate
+//        switch game {
+//        case .ColoredSquares: isAcitive_ColoredSquares = true
+//        }
+//
+//        func deactivateAllGames () {
+//            isAcitive_ColoredSquares = false
+//        }
+//    }
     
 }
 
