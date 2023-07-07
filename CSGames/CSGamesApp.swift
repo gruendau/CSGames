8167758b5289cda11a033ce8ea5146fca58e26a7
volker <101987062+gruendau@users.noreply.github.com>
2023-07-07 18:31:02 +0200

    Removed 2x @StateObjects.

--- CSGames/CSGamesApp.swift
+++ CSGames/CSGamesApp.swift
@@ -10,16 +10,12 @@ import SwiftUI
 
 @main
 struct CSGamesApp: App {
-    
-    @StateObject var csm = CSModel()
-    @StateObject var vp = CSSettings()
+
     @StateObject var s = Settings()
     
     var body: some Scene {
         WindowGroup {
             Main()
-                .environmentObject(csm)
-                .environmentObject(vp)
                 .environmentObject(s)
         }
     }
