80714cd4c516a0347b2828e9e38131934115b3f1
volker <101987062+gruendau@users.noreply.github.com>
2023-07-07 20:07:11 +0200

    Added content to AppData.

--- CSGames/Games/ColoredSquares/Views/Info/CSAppData.swift
+++ CSGames/Games/ColoredSquares/Views/Info/CSAppData.swift
@@ -8,8 +8,41 @@
 import SwiftUI
 
 struct CSAppData: View {
+    
+    @EnvironmentObject var csm: CSViewModel
+    @EnvironmentObject var set: CSSettings
+    
     var body: some View {
-        Text("App Data")
+        
+        ZStack(alignment: .leading) {
+            
+            // 1.Layer
+            Color.black
+            
+            // 2. Layer
+            VStack(alignment: .leading) {
+                Text("App Data")
+                    .font(.title3)
+                    .bold()
+                    .padding(.bottom)
+                
+                VStack(alignment: .leading) {
+                    Text("Text ...")
+                        .padding(.vertical, 1)
+
+                }
+                .padding(.leading, 15)
+                
+                Spacer()
+            }
+            .padding(.leading)
+            .padding(.top, -35)
+            
+        }
+        .padding(.horizontal, 20)
+        .foregroundColor(set.colorForeground)
+        .background(set.colorBackground)
+        
     }
 }
 
@@ -17,5 +50,7 @@ struct CSAppData: View {
 struct CSAppData_Previews: PreviewProvider {
     static var previews: some View {
         CSAppData()
+            .environmentObject(CSViewModel())
+            .environmentObject(CSSettings())
     }
 }
