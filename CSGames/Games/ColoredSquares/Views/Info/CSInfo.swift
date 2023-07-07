ab7bf8cef317ff57a629d776877b022ac1167afb
volker <101987062+gruendau@users.noreply.github.com>
2023-07-08 01:15:39 +0200

    REMOVED NAVIGATION CODE from CSInfo.swift.

--- CSGames/Games/ColoredSquares/Views/Info/CSInfo.swift
+++ CSGames/Games/ColoredSquares/Views/Info/CSInfo.swift
@@ -9,18 +9,6 @@ import SwiftUI
 
 struct CSInfo: View {
 
-    // https://developer.apple.com/library/archive/documentation/UserExperience/Conceptual/TableView_iPhone/TableViewAPIOverview/TableViewAPIOverview.html
-
-    init () {
-        // Remove separator lines
-        UITableView.appearance().separatorColor = .clear
-        // Set background color
-        UITableView.appearance().backgroundColor = .black
-
-        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.white]
-
-    }
-
     @EnvironmentObject var csm: CSViewModel
     @EnvironmentObject var set: CSSettings
 
@@ -76,45 +64,6 @@ struct CSInfo: View {
             
         }
 
-
-
-
-//            NavigationView {
-//
-//                List {
-//
-//                    NavigationLink(destination: CSAppUserInstructions()) {
-//                        ItemCell(item: "User Instructions")
-//                    }
-//                    .listRowBackground(listRowBGColor)
-//
-//                    NavigationLink(destination: CSAppData()) {
-//                        ItemCell(item: "Data")
-//                    }
-//                    .listRowBackground(listRowBGColor)
-//
-//                    NavigationLink(destination: CSAppStatistics()) {
-//                        ItemCell(item: "Statistics")
-//                    }
-//                    .listRowBackground(listRowBGColor)
-//
-//                    NavigationLink(destination: CSAppCustomization()) {
-//                        ItemCell(item: "Custumization")
-//                    }
-//                    .listRowBackground(listRowBGColor)
-//
-//                }
-//                .navigationBarHidden(true)
-//                .foregroundColor(set.colorForeground)
-//                .padding(.top, -25)
-//
-//            }
-//            .navigationBarBackButtonHidden(true)
-//
-//            Spacer()
-//
-//        }
-
     }
 }
 
@@ -126,95 +75,3 @@ struct CSInfo_Previews: PreviewProvider {
             .environmentObject(CSSettings())
     }
 }
-
-
-//struct NavigationConfigurator: UIViewControllerRepresentable {
-//    var configure: (UINavigationController) -> Void = { _ in }
-//
-//    func makeUIViewController(context: UIViewControllerRepresentableContext<NavigationConfigurator>) -> UIViewController {
-//        UIViewController()
-//    }
-//    func updateUIViewController(_ uiViewController: UIViewController, context: UIViewControllerRepresentableContext<NavigationConfigurator>) {
-//        if let nc = uiViewController.navigationController {
-//            self.configure(nc)
-//        }
-//    }
-//
-//}
-
-//struct ItemCell: View {
-//    // https://www.hackingwithswift.com/forums/swiftui/background-color-of-a-list-make-it-clear-color/3379
-//
-//    var item: String
-//
-//    var body: some View {
-//
-//        Text(item)
-//            .foregroundColor(.white)
-//
-//    }
-//}
-
-
-
-//struct CSInfo: View {
-//
-//    // https://developer.apple.com/library/archive/documentation/UserExperience/Conceptual/TableView_iPhone/TableViewAPIOverview/TableViewAPIOverview.html
-//
-//    init () {
-//        // Remove separator lines
-//        UITableView.appearance().separatorColor = .clear
-//        // Set background color
-//        UITableView.appearance().backgroundColor = .black
-//
-//        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.white]
-//
-//    }
-//
-//    @EnvironmentObject var csm: CSViewModel
-//    @EnvironmentObject var set: CSSettings
-//
-//    @State private var showSquareboxInfo = false
-//
-//    let listRowBGColor: Color = .black
-//
-//    var body: some View {
-//
-//        NavigationView {
-//
-//            List {
-//
-//                NavigationLink(destination: CSAppUserInstructions()) {
-//                    ItemCell(item: "User Instructions")
-//                }
-//                .listRowBackground(listRowBGColor)
-//
-//                NavigationLink(destination: CSAppData()) {
-//                    ItemCell(item: "Data")
-//                }
-//                .listRowBackground(listRowBGColor)
-//
-//                NavigationLink(destination: CSAppStatistics()) {
-//                    ItemCell(item: "Statistics")
-//                }
-//                .listRowBackground(listRowBGColor)
-//
-//                NavigationLink(destination: CSAppCustomization()) {
-//                    ItemCell(item: "Custumization")
-//                }
-//                .listRowBackground(listRowBGColor)
-//
-//            }
-//            .navigationBarHidden(true)
-//            .foregroundColor(set.colorForeground)
-//            .padding(.top, -25)
-//
-//        }
-//        .navigationBarBackButtonHidden(true)
-//
-//        Spacer()
-//
-//    }
-//
-//}
-
