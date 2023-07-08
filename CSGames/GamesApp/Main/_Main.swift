8779ac9355887f4d8e177d7fa21560fba7cf7e30
volker <101987062+gruendau@users.noreply.github.com>
2023-07-08 23:53:18 +0200

    Coded new Main.

--- CSGames/GamesApp/Main/_Main.swift
+++ CSGames/GamesApp/Main/_Main.swift
@@ -7,14 +7,55 @@
 
 import SwiftUI
 
-struct _Main: View {
+struct _Main<Content: View, Links: View>: View {
+    
+    @EnvironmentObject var set: Settings
+
+    let content: Content
+    let links: Links
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            
+            // 1. Layer
+            // Background
+            Background()
+                
+            // 2. Layer
+            // Content (title, content, copyright)
+            VStack {
+                
+                // Title
+                Title()
+                
+                // Content
+                VStack {
+                    content
+                }
+                .foregroundColor(set.colorForeground)
+                .padding(.top, set.paddingContentTop)
+                
+                Spacer()
+            
+                // Copyright
+                Copyright()
+
+            }
+            
+            // 3. Layer
+            // Chosen game
+            links
+                
+        }
+        .edgesIgnoringSafeArea(.all)
+        
     }
 }
 
 struct _Main_Previews: PreviewProvider {
     static var previews: some View {
-        _Main()
+        _Main(content: Text("Content"), links: Text("Links"))
+            .environmentObject(Settings())
     }
 }
