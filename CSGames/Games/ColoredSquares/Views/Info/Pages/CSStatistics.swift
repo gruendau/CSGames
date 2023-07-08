5baa2aca431fe1652a5305d4485d934155cd23ef
volker <101987062+gruendau@users.noreply.github.com>
2023-07-08 13:26:11 +0200

    Refactured CSStatistics.

--- CSGames/Games/ColoredSquares/Views/Info/Pages/CSStatistics.swift
+++ CSGames/Games/ColoredSquares/Views/Info/Pages/CSStatistics.swift
@@ -8,43 +8,35 @@
 import SwiftUI
 
 struct CSStatistics: View {
-    
     @EnvironmentObject var set: CSSettings
-    
     var body: some View {
-        
         _CSInfoPage(content: InfoContent())
-        
     }
 }
 
-
-
-
 struct InfoContent: View {
-
     @EnvironmentObject var set: CSSettings
-
     var body: some View {
-
+        
         Group {
-
+            // Content Start ---------------
+            
             CSInfoTitle(title: "Statistics",
                         show: $set.showStatistics)
-
+            
             VStack(alignment: .leading) {
                 Text("Text ...")
                     .padding(.vertical, 1)
-
+                
             }
-
+            
             Spacer()
+            
+            // Content End -----------------
         }
-
+        
     }
 }
-
-
 
 
 struct CSStatistics_Previews: PreviewProvider {
@@ -53,3 +45,4 @@ struct CSStatistics_Previews: PreviewProvider {
             .environmentObject(CSSettings())
     }
 }
+
