53e9e1334bde3fa89269480108409a2c02a3de9b
volker <101987062+gruendau@users.noreply.github.com>
2023-07-09 00:59:25 +0200

    Added properties to Settings.

--- CSGames/GamesApp/Settings.swift
+++ CSGames/GamesApp/Settings.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 class Settings: ObservableObject {
     
+    // MARK: Main
     // Texts
     let textTitle = "CSGames"
     let textCopyright = "© 2023 by volker"
@@ -23,8 +24,26 @@ class Settings: ObservableObject {
     let paddingContentTop = CGFloat(30)
     let paddingCopyrightBottom = CGFloat(18)
     
+
+    // MARK: Games
     // Games
     @Published var showColoredSquares = false
+    
+    
+    // MARK: Info
+    // Info
+    let paddingInfoLeading = CGFloat(30)
+    let paddingInfoTop = CGFloat(25)
+    let paddingInfoHorizontal = CGFloat(20)
+    
+    // AppInfo
+    @Published var showAppInfo = false
+    
+    @Published var showUserInstructions = false
+    @Published var showData = false
+    @Published var showStatistics = false
+    @Published var showCustomization = false
+    @Published var showAbout = false
     
 }
 
