93cd25c7c0edb9a613aad5c1c33c25b16c60d25c
volker <101987062+gruendau@users.noreply.github.com>
2023-07-09 23:13:00 +0200

    Changed output design.

--- CSGames/Games/ColoredSquares/Views/Info/Pages/CSData.swift
+++ CSGames/Games/ColoredSquares/Views/Info/Pages/CSData.swift
@@ -23,88 +23,133 @@ private struct Content: View {
         Group {
             // CONTENT START ---------------------
             
-            let offset = CGFloat(100)
+            let offset = CGFloat(150)
             
-            CSInfoTitle(title: "Game Data",
+            CSInfoTitle(title: "Data",
                         show: $set.showData)
             
             VStack(alignment: .leading) {
                 Group {
-                    Text("Squarebox size")
+                    Text("Actual Values")
                         .foregroundColor(.blue)
                         .bold()
-                    VStack(alignment: .leading) {
-                        CSDataText(key: "size",
-                                   value: String(Int(csm.size_Squarebox)),
-                                   unit: "px",
-                                   widthKeyValue: offset,
-                                   colorValue: .blue)
-                        CSDataText(key: "min",
-                                   value: String(Int(csm.minSize_Squarebox)),
-                                   unit: "px",
-                                   widthKeyValue: offset,
-                                   colorValue: .blue)
-                        CSDataText(key: "max",
-                                   value: String(Int(csm.maxSize_Squarebox)),
-                                   unit: "px",
-                                   widthKeyValue: offset,
-                                   colorValue: .blue)
-                        CSDataText(key: "step",
-                                   value: "\(String(Int(csm.stepSize_Squarebox)))",
-                                   unit: "px",
-                                   widthKeyValue: offset,
-                                   colorValue: .blue)
-                        
-                    }
-                    .padding(.leading)
-                    Text("")
-                }
-                Group {
-                    Text("Square size")
-                        .foregroundColor(.blue)
-                        .bold()
-                    VStack(alignment: .leading) {
-                        CSDataText(key: "size",
-                             value: String(Int(csm.size_Square)),
-                             unit: "px",
-                             widthKeyValue: offset,
-                             colorValue: .blue)
-                    }
-                    .padding(.leading)
-                    Text("")
-                }
-                Group {
-                    Text("Squares per edge")
-                        .foregroundColor(.blue)
-                        .bold()
-                    VStack(alignment: .leading) {
-                        CSDataText(key: "number",
-                             value: String(Int(csm.number_SquaresPerEdge)),
-                             unit: "",
-                             widthKeyValue: offset,
-                             colorValue: .blue)
-                        CSDataText(key: "min",
-                             value: String(Int(csm.minNumber_SquaresPerEdge)),
-                             unit: "",
-                             widthKeyValue: offset,
-                             colorValue: .blue)
-                        CSDataText(key: "max",
-                             value: String(Int(csm.maxNumber_SquaresPerEdge)),
-                             unit: "",
-                             widthKeyValue: offset,
-                             colorValue: .blue)
-                        CSDataText(key: "step",
-                             value: String(Int(csm.stepNumber_SquaresPerEdge)),
-                             unit: "",
-                             widthKeyValue: offset,
-                             colorValue: .blue)
-                        
-                    }
-                    .padding(.leading)
-                    Text("")
-                }
-                Group {
                     
+                    Text("")
+                    
+                    VStack(alignment: .leading) {
+                        Text("Squares")
+                            .foregroundColor(.blue)
+                        
+                        VStack(alignment: .leading) {
+                            
+                            CSDataText(key: "total",
+                                       value: String(Int(csm.number_SquaresPerEdge * csm.number_SquaresPerEdge)),
+                                       unit: "",
+                                       widthKeyValue: offset,
+                                       colorValue: .blue)
+                            
+                            CSDataText(key: "per edge",
+                                       value: String(Int(csm.number_SquaresPerEdge)),
+                                       unit: "",
+                                       widthKeyValue: offset,
+                                       colorValue: .blue)
+                            
+                            CSDataText(key: "size",
+                                       value: String(Int(csm.size_Square)),
+                                       unit: "px",
+                                       widthKeyValue: offset,
+                                       colorValue: .blue)
+                            
+                        }
+                        .padding(.leading)
+                        
+                        Text("")
+                        
+                        Text("Squarebox")
+                            .foregroundColor(.blue)
+                        
+                        VStack(alignment: .leading) {
+                            
+                            CSDataText(key: "size",
+                                       value: String(Int(csm.size_Squarebox)),
+                                       unit: "px",
+                                       widthKeyValue: offset,
+                                       colorValue: .blue)
+                            
+                        }
+                        .padding(.leading)
+                        
+                    }
+                    .padding(.leading)
+                    
+                }
+                
+                Text("")
+                
+                Group {
+                    Text("Setpoints")
+                        .foregroundColor(.blue)
+                        .bold()
+                    
+                    Text("")
+                    
+                    VStack(alignment: .leading) {
+                        Text("Squarebox")
+                            .foregroundColor(.blue)
+                        
+                        VStack(alignment: .leading) {
+                            
+                            CSDataText(key: "min",
+                                       value: String(Int(csm.minSize_Squarebox)),
+                                       unit: "px",
+                                       widthKeyValue: offset,
+                                       colorValue: .blue)
+                            
+                            CSDataText(key: "max",
+                                       value: String(Int(csm.maxSize_Squarebox)),
+                                       unit: "px",
+                                       widthKeyValue: offset,
+                                       colorValue: .blue)
+                            
+                            CSDataText(key: "step",
+                                       value: "\(String(Int(csm.stepSize_Squarebox)))",
+                                       unit: "px",
+                                       widthKeyValue: offset,
+                                       colorValue: .blue)
+                            
+                        }
+                        .padding(.leading)
+                        
+                        Text("")
+                        
+                        Text("Squares per Edge")
+                            .foregroundColor(.blue)
+                        
+                        VStack(alignment: .leading) {
+                            
+                            CSDataText(key: "min",
+                                       value: String(Int(csm.minNumber_SquaresPerEdge)),
+                                       unit: "",
+                                       widthKeyValue: offset,
+                                       colorValue: .blue)
+                            
+                            CSDataText(key: "max",
+                                       value: String(Int(csm.maxNumber_SquaresPerEdge)),
+                                       unit: "",
+                                       widthKeyValue: offset,
+                                       colorValue: .blue)
+                            
+                            CSDataText(key: "step",
+                                       value: String(Int(csm.stepNumber_SquaresPerEdge)),
+                                       unit: "",
+                                       widthKeyValue: offset,
+                                       colorValue: .blue)
+                            
+                        }
+                        .padding(.leading)
+                        
+                    }
+                    .padding(.leading)
                 }
                 
             }
